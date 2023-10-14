0b4354213d1b28a781452b7a11a3afd8b46ebc5a
jevtasite <jevta.site@gmail.com>
2023-10-14 22:14:44 +0200

    Removed icon hover, changed font size
    Removed icon hover from footer, changed font size in gallery and product

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 function openGmail() {
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-window.location.href = "mailto:youremail@gmail.com";
+window.location.href = "mailto:jevta.site@gmail.com";
  } 
 else {
 // If on a desktop, open the Gmail website
- window.open("https://mail.google.com/mail/?view=cm&source=mailto&to=jevta@mail.com");
+ window.open("https://mail.google.com/mail/?view=cm&source=mailto&to=jevta.site@gmail.com");
  }
 }
